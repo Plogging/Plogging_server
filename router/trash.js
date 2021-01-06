71f5294bcefa15012da4ce9a9fb9542c00b42712
xowns4817 <xowns4817@naver.com>
2021-01-06 14:28:26 +0900

    산책이력 삭제 api 구현

--- router/trash.js
+++ router/trash.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors=require('cors');
-const { default: axios } = require('axios');
 const fs = require('fs');
 const util = require('../util/common.js');
 
@@ -37,9 +36,11 @@ const TrashInferface = function(config) {
     // 쓰레기 관련 api 구현
     router.get("/", (req, res) => this.readTrash(req, res));// read
     router.post("/", upload.single('trashImg'), (req, res) => this.writeTrash(req, res)); // create
+    router.delete("/", (req, res) => this.deleteTrash(req,res)); // delete
     return this.router;
 };
 
+// 산책 이력 조회
 TrashInferface.prototype.readTrash = async function(req, res) {
     console.log("trash read api !");
 
@@ -58,12 +59,12 @@ TrashInferface.prototype.readTrash = async function(req, res) {
     }
 }
 
+// 산책 이력 등록
 TrashInferface.prototype.writeTrash = async function(req, res) {
     console.log("trash write api !");
 
     let trashObj = { };
     let userId = req.body.userId;
-    let trashImg = req.file;
 
     trashObj.meta = { };
     trashObj.meta.user_id = userId;
@@ -73,25 +74,33 @@ TrashInferface.prototype.writeTrash = async function(req, res) {
     trashObj.meta.flogging_time = 20;
 
     trashObj.meta.trash_img = `http://localhost:20000/trash/${userId}/flogging_${trashObj.meta.create_time}.PNG`; // file server 찔러서 이미지 가져옴
-    
 
     trashObj.trash_list = [ ];
     trashObj.trash_list[0] = {"trash_type": 2, "pick_count": 100};
     trashObj.trash_list[1] = {"trash_type": 1, "pick_count": 200};
 
     let mongoConnection = null;
-   // const form = new FormData(); // multipart/form-data
-   // form.append('userId', trashObj.meta.user_id);
-   // form.append('profileImge', profileImg)
-
     try {
-    
-    // file server post request -> node 내부에서 처리
-    //const response = await axios.post('http://localhost:8080/upload/profileImg', form);
-    //console.log(response);
-    
         mongoConnection = this.MongoPool.db('test');
         await mongoConnection.collection('trash').insertOne(trashObj);
+    } catch(e) {
+        console.log(e);
+    } finally {
+        mongoConnection=null;
+    }
+}
+
+// 산책 이력 삭제 -> 유저가 해당 이력을 삭제하거나, 회원 탈퇴했을때
+TrashInferface.prototype.deleteTrash = async function(req, res) {
+    console.log("trash delete api !");
+
+    let userId = req.body.userId;
+    let query = {"meta.user_id": userId};
+
+    let mongoConnection = null;
+    try {
+        mongoConnection = this.MongoPool.db('test');
+        await mongoConnection.collection('trash').deleteMany(query);
     } catch(e) {
         console.log(e);
     } finally {
