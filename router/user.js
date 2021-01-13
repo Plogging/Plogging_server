98bbe2079ff207e3f8b6401a15abfff8413a65be
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-01-13 14:35:49 +0900

    로그인 부분 트랜잭션 추가

--- router/user.js
+++ router/user.js
@@ -43,71 +43,71 @@ const UserInterface = function(config) {
 
 UserInterface.prototype.signIn = async function(req, res) {
 
-    const pool = this.pool;
     let returnResult = { rc: 200, rcmsg: "success" };
     const user = req.body;
-    try {
-        // case no parameter
-        if(!user.type || !user.email) {
-            returnResult.rc = 400;
-            returnResult.rcmsg = "no parameter";
-            res.status(400).send(returnResult);
-            return;
-        }
-        
-        try {
-            // search userId in DB
-            const userId = user.email + ':' + user.type
-            const findUserQuery = `SELECT * FROM ${USER_TABLE} WHERE user_id = ?`;
-            const findUserValues = [userId];
-            pool.query(findUserQuery, findUserValues, function(err, result) {
-                if (result.length === 0) {
-                    // set nickname
-                    let nickName = user.displayName;
-                    if(!user.displayName) nickName = "쓰담이";
 
-                    // set userImg
-                    let userImg = "https://i.pinimg.com/564x/d0/be/47/d0be4741e1679a119cb5f92e2bcdc27d.jpg";
-                    const createDateline = util.getCurrentDateTime();
-                    const createUserQuery = `INSERT INTO ${USER_TABLE}(user_id, display_name, profile_img, type, email, update_datetime, create_datetime) VALUES(?, ?, ?, ?, ?, ?, ?)`;
-                    const createUserValues = [userId, nickName, userImg, user.type, user.email, createDateline, createDateline];
-                    pool.query(createUserQuery, createUserValues, function(err, result){
-                        if(err) {
-                            returnResult.rc = 500;
-                            returnResult.rcmsg = "Failed to creating user";
-                            res.send(returnResult);
-                        }else{
-                            req.session.key = userId
-                            returnResult.rc = 200;
-                            returnResult.rcmsg = "Success creating user";
-                            returnResult.session = req.session;
-                            returnResult.userImg = userImg;
-                            returnResult.userName = result[0].display_name;
-                            console.log(result)
-                            res.send(returnResult);
-                        };
-                    });
-                }else{
-                    req.session.key = userId
-                    returnResult.rc = 200;
-                    returnResult.rcmsg = "Success getting user";
-                    returnResult.session = req.session.key;
-                    returnResult.userImg = result[0].profile_img;
-                    returnResult.userName = result[0].display_name;
-                    res.send(returnResult);
-                };
-            });
-            
-        } catch (error) {
-            returnResult.rcmsg = error.message;
-            returnResult.rc = 600;
-            res.send(returnResult);
-        }
-    } catch (error) { 
-        returnResult.rc = 500;
-        returnResult.rcmsg = error.message;
-        res.send(returnResult);
+    // case no parameter
+    if(!user.type || !user.email) {
+        returnResult.rc = 400;
+        returnResult.rcmsg = "no parameter";
+        res.status(400).send(returnResult);
+        return;
     }
+    // search userId in DB
+    const userId = user.email + ':' + user.type
+    const findUserQuery = `SELECT * FROM ${USER_TABLE} WHERE user_id = ?`;
+    const findUserValues = [userId];
+
+    this.pool.getConnection(function(err, conn){
+        conn.beginTransaction();
+        conn.execute(findUserQuery, findUserValues, function(err, result) {
+            if(err){
+                returnResult.rc = 600;
+                returnResult.rcmsg = err.message;
+                res.send(returnResult);
+                conn.rollback()
+                return;
+            }
+            if (result.length === 0) {
+                // set nickname
+                let nickName = user.displayName;
+                if(!user.displayName) nickName = "쓰담이";
+                
+                // set userImg
+                let userImg = "https://i.pinimg.com/564x/d0/be/47/d0be4741e1679a119cb5f92e2bcdc27d.jpg";
+                const createDateline = util.getCurrentDateTime();
+                const createUserQuery = `INSERT INTO ${USER_TABLE}(user_id, display_name, profile_img, type, email, update_datetime, create_datetime) VALUES(?, ?, ?, ?, ?, ?, ?)`;
+                const createUserValues = [userId, nickName, userImg, user.type, user.email, createDateline, createDateline];
+                conn.execute(createUserQuery, createUserValues, function(err, result){
+                    if(err) {
+                        returnResult.rc = 600;
+                        returnResult.rcmsg = "Failed to creating user";
+                        res.send(returnResult);
+                        conn.rollback()
+                        return;
+                    }
+                    req.session.id = userId
+                    returnResult.rc = 200;
+                    returnResult.rcmsg = "Success creating user";
+                    returnResult.session = req.session.id;
+                    returnResult.userImg = userImg;
+                    returnResult.userName = nickName;
+                    res.send(returnResult);
+                    conn.commit()
+                });
+            }else{
+                req.session.id = userId
+                returnResult.rc = 200;
+                returnResult.rcmsg = "Success getting user";
+                returnResult.session = req.session.id;
+                returnResult.userImg = result[0].profile_img;
+                returnResult.userName = result[0].display_name;
+                res.send(returnResult);
+                conn.commit()
+            };
+        });
+        conn.release()
+    });
 }
 
 UserInterface.prototype.update = async function(req, res) {
@@ -126,9 +126,9 @@ UserInterface.prototype.update = async function(req, res) {
         }else{
             const profileImg = req.file.path
             let updateUserQuery = `UPDATE ${USER_TABLE} SET display_name = ?, profile_img = ?, update_datetime = ? WHERE user_id = ?`
-            let updateUserValues = [user.display_name, profileImg, currentTime, req.session.key];
+            let updateUserValues = [user.display_name, profileImg, currentTime, req.session.id];
             
-            pool.query(updateUserQuery, updateUserValues, function(err, result) {
+            pool.execute(updateUserQuery, updateUserValues, function(err, result) {
                 if(err){
                     returnResult.rc = 600;
                     returnResult.rcmsg = err.message;
