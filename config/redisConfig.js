8410aca4268f4481816753712c00a1ef1c0cbb42
Fuzzy JH <jukoh01@coupang.com>
2021-01-16 22:02:42 +0900

    잡기술 넣음

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,7 +1,6 @@
 const redis = require('ioredis');
 const redisInfo = process.env.REDIS_INFO;
-const ip = redisInfo.split(":")[0];
-const port = redisInfo.split(":")[1];
+const [ip, port] = redisInfo.split(":");
 const jasypt = require('../util/common_jasypt.js');
 const redisPassword = jasypt.decrypt(process.env.REDIS_PASSWORD);
 
