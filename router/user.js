7f8f0ba603857bbd0440a251108cb1473d5db414
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-01-20 15:22:45 +0900

    swagger validation 적용

--- router/user.js
+++ router/user.js
@@ -25,7 +25,6 @@ const UserInterface = function(config) {
             destination: function (req, file, cb) {
             const userId = req.userId; // 세션체크 완료하면 값 받아옴
 	        const dir = `${filePath}${userId}`;
-    
             if (!fs.existsSync(dir)){
                 fs.mkdirSync(dir);
             }
@@ -39,32 +38,23 @@ const UserInterface = function(config) {
         })
 
     // 유저 관련 api 구현
-    router.post('', (req, res) => this.signIn(req, res));
-    router.get('', (req, res) => this.getUserInfo(req, res));
-    router.get('/sign-out', (req, res) => this.signOut(req, res));
-    router.put('', upload.single('profile_img'), (req, res) => this.update(req, res));
-    router.delete('', (req, res) => this.withdrawal(req, res));
+    router.post('', swaggerValidation.validate, (req, res) => this.signIn(req, res));
+    router.get('', swaggerValidation.validate, (req, res) => this.getUserInfo(req, res));
+    router.get('/sign-out', swaggerValidation.validate, (req, res) => this.signOut(req, res));
+    router.put('', upload.single('profileImg'), swaggerValidation.validate, (req, res) => this.update(req, res));
+    router.delete('', swaggerValidation.validate, (req, res) => this.withdrawal(req, res));
     return this.router;
 };
 
 UserInterface.prototype.signIn = async function(req, res) {
 
-    let returnResult = { rc: 500, rcmsg: "server error" };
+    let returnResult = {};
     const secretKey = req.body.secretKey;
     const [userEmail, userType] = req.body.userId.split(":");
     const userName = req.body.userName;
 
-    // case no parameter
-    if(!userEmail || !userType) {
-        returnResult.rc = 400;
-        returnResult.rcmsg = "no parameter";
-        res.status(400).send(returnResult);
-        return;
-    }
-    if(userType.toLowerCase() === 'custom' && !secretKey){
-        returnResult.rc = 400;
-        returnResult.rcmsg = "no parameter";
-        res.status(400).send(returnResult);
+    if(userType.toLowerCase() === 'custom' && !secretKey){ 
+        res.sendStatus(400);
         return;
     }
     
@@ -96,52 +86,40 @@ UserInterface.prototype.signIn = async function(req, res) {
                 
                 conn.execute(createUserQuery, createUserValues, function(err, result){
                     if(err) {
-                        returnResult.rc = 600;
-                        returnResult.rcmsg = err.message;
-                        res.send(returnResult);
+                        res.sendStatus(500);
                         conn.rollback();
                     }else{
                         req.session.userId = userId;
-                        returnResult.rc = 200;
-                        returnResult.rcmsg = "Success creating user";
                         returnResult.session = req.session.id;
                         returnResult.userImg = userImg;
                         returnResult.userName = userName;
-                        res.send(returnResult);
+                        res.json(returnResult);
                         conn.commit();
                     }
                 });
                 if(err) {
-                    returnResult.rc = 600;
-                    returnResult.rcmsg = err.message;
-                    res.send(returnResult);
+                    res.sendStatus(500);
                     conn.rollback();
                 }
             }else{
                 if(userType.toLowerCase() === 'custom'){
                     const hashedPassword = crypto.pbkdf2Sync(secretKey, result[0].salt, 10000, 64, 'sha512').toString('base64');
                     if(hashedPassword != result[0].hash_password){
-                        returnResult.rc = 401;
-                        returnResult.rcmsg = "password error";
-                        res.send(returnResult);
+                        res.sendStatus(401);
                         conn.rollback();
                         return;
                     }
                 }
-                req.session.userId = userId
-                returnResult.rc = 200;
-                returnResult.rcmsg = "Success getting user";
+                req.session.userId = userId;
                 returnResult.session = req.session.id;
                 returnResult.userImg = result[0].profile_img;
                 returnResult.userName = result[0].display_name;
-                res.send(returnResult);
+                res.json(returnResult);
                 conn.commit();
             };
         });
         if(err){
-            returnResult.rc = 600;
-            returnResult.rcmsg = err.message;
-            res.send(returnResult);
+            res.sendStatus(500);
             conn.rollback();
         }
         conn.release();
@@ -152,119 +130,69 @@ UserInterface.prototype.signIn = async function(req, res) {
 UserInterface.prototype.getUserInfo = async function(req, res) {
     
     const pool = this.pool;
-    let returnResult = { rc: 200, rcmsg: 'success' };
-    console.log(req.session.userId);
-    if(!req.session){
-        returnResult.rc = 401;
-        returnResult.rcmsg = "no header key"
-        return;
-    }
+    let returnResult = {};
     try {
         const getUserQuery = `SELECT * FROM ${USER_TABLE} WHERE user_id = ?`;
         const getUserValues = [req.session.userId];
         
         pool.execute(getUserQuery, getUserValues, function(err, result) {
-            console.log(result);
             if(result.length){
-                returnResult.rc = 200;
-                returnResult.rcmsg = 'success';
                 returnResult.userId = result[0].user_id;
                 returnResult.userImg = result[0].profile_img;
                 returnResult.userName = result[0].display_name;
-                res.send(returnResult);
+                res.json(returnResult);
             }else{
-                returnResult.rc = 500;
-                returnResult.rcmsg = 'Getting user error';
-                res.send(returnResult);
+                res.sendStatus(500);
             }
             if(err){
-                returnResult.rc = 600;
-                returnResult.rcmsg = err.message;
-                res.send(returnResult);
+                res.sendStatus(500);
             }
         })
     } catch (error) {
-        returnResult.rc = 500;
-        returnResult.rcmsg = error.message;
-        res.send(returnResult);
+        res.sendStatus(500);
     }
 }
 
 UserInterface.prototype.update = async function(req, res) {
-    
+    console.log(req.body.displayName)
     const pool = this.pool;
-    let returnResult = { rc: 200, rcmsg: "success" };
-    const user = req.body;
-    const userId = req.userId;
+    let returnResult = {};
+    const displayName = req.body.displayName;
     const currentTime = util.getCurrentDateTime();
-    if(!req.session){
-        returnResult.rc = 401;
-        returnResult.rcmsg = "no header key"
-        return;
-    }
     try {
-        if(!user.display_name || !req.file) {
-            returnResult.rc = 400;
-            returnResult.rcmsg = "no parameter";
-            res.status(400).send(returnResult);
-            return;
-        }else{
-            const profileImg = process.env.SERVER_REQ_INFO + '/' + req.file.path.split("/mnt/Plogging_server/images/")[1];
-            const updateUserQuery = `UPDATE ${USER_TABLE} SET display_name = ?, profile_img = ?, update_datetime = ? WHERE user_id = ?`
-            const updateUserValues = [user.display_name, profileImg, currentTime, req.session.userId];
-            
-            pool.execute(updateUserQuery, updateUserValues, function(err, result) {
-                if(result.affectedRows){
-                    returnResult.rc = 200;
-                    returnResult.rcmsg = "Success user updated";
-                    returnResult.displayName = user.display_name;
-                    returnResult.profile_img = profileImg;
-                    res.send(returnResult);
-                }else{
-                    returnResult.rc = 500;
-                    returnResult.rcmsg = "user update error";
-                    res.send(returnResult);
-                }
-                if(err){
-                    returnResult.rc = 600;
-                    returnResult.rcmsg = err.message;
-                    res.send(returnResult);
-                }
-            })
-        }
+        const profileImg = process.env.SERVER_REQ_INFO + '/' + req.file.path.split("/mnt/Plogging_server/images/")[1];
+        const updateUserQuery = `UPDATE ${USER_TABLE} SET display_name = ?, profile_img = ?, update_datetime = ? WHERE user_id = ?`
+        const updateUserValues = [displayName, profileImg, currentTime, req.session.userId];
+        
+        pool.execute(updateUserQuery, updateUserValues, function(err, result) {
+            if(result.affectedRows){
+                returnResult.displayName = displayName;
+                returnResult.profileImg = profileImg;
+                res.send(returnResult);
+            }else{
+                res.sendStatus(500);
+            }
+            if(err) {
+                res.sendStatus(500);
+            }
+        })
     } catch (error) {
-        returnResult.rc = 500;
-        returnResult.rcmsg = error.message;
-        res.send(returnResult);
+        res.sendStatus(500);
     }
 }
 
 UserInterface.prototype.signOut = async function(req, res) {
-    let returnResult = { rc: 200, rcmsg: "success sign out" };
-    if(!req.session){
-        returnResult.rc = 401;
-        returnResult.rcmsg = "no header key"
-        return;
-    }
     req.session.destroy(function(err) {
         if(err) {
-            returnResult.rc = 500;
-            returnResult.rcmsg = "server err"
-            res.send(returnResult);
+            res.sendStatus(500);
         }else {
-            res.send(returnResult);
+            res.sendStatus(200);
         }
     })
 }
 
 UserInterface.prototype.withdrawal = async function(req, res) {
-    let returnResult = { rc: 200, rcmsg: "success withdrawal" };
     const userId = req.session.userId;
-    if(!req.session){
-        returnResult.rc = 401;
-        returnResult.rcmsg = "no header key"
-        return;
-    }
     const deleteUserQuery = `DELETE FROM ${USER_TABLE} WHERE user_id = ?`;
     const deleteUserValues = [userId];
     const mongoConnection = this.MongoPool.db('plogging');
@@ -276,9 +204,7 @@ UserInterface.prototype.withdrawal = async function(req, res) {
                 mongoConnection.collection('record')
                     .deleteMany({"meta.user_id": userId}, function(err, result) {
                         if(err){
-                            returnResult.rc = 500;
-                            returnResult.rcmsg = 'delete user error';
-                            res.send(returnResult);
+                            res.sendStatus(500);
                             conn.rollback();
                         }else{
                             try {
@@ -288,36 +214,26 @@ UserInterface.prototype.withdrawal = async function(req, res) {
                                 }
                                 // 해당 산책의 점수 랭킹점수 삭제
                                 //await this.redisAsyncZrem(queryKey, userId);
-                                returnResult.rc = 200;
-                                returnResult.rcmsg = 'success withdrawal';
-                                res.send(returnResult);
+                                res.sendStatus(200);
                                 req.session.destroy();
                                 conn.commit();
                             } catch (error) {
-                                returnResult.rc = 510;
-                                returnResult.rcmsg = error.message;
-                                res.send(returnResult);
+                                res.sendStatus(500);
                                 conn.rollback();
                             }
                         }
                 });
                 
             }else{
-                returnResult.rc = 500;
-                returnResult.rcmsg = 'delete user error';
-                res.send(returnResult);
+                res.sendStatus(500);
             };
             if(err){
-                returnResult.rc = 600;
-                returnResult.rcmsg = err.message;
-                res.send(returnResult);
+                res.sendStatus(500);
             };
             
         });
         if(err){
-            returnResult.rc = 500;
-            returnResult.rcmsg = err.message;
-            res.send(returnResult);
+            res.sendStatus(500);
         };
         conn.release();
     });
