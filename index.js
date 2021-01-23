8e0501c5fee6ba5370dfcc599b926151b19e60d4
Fuzzy JH <jukoh01@coupang.com>
2021-01-23 21:27:57 +0900

    오타 수정

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const redisClient = require("./config/redisConfig.js");
 const MongoClient = require("./config/mongoConfig.js");
 const swaggerValidation = require('./util/validator.js');
 const logger = require("./util/logger.js")("index.js");
-const logHelper = require("../util/logHelper.js");
+const logHelper = require("./util/logHelper.js");
 
 const session = require('express-session');
 const redisStore = require('connect-redis')(session);
