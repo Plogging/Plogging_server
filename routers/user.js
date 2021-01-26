05003bfe639d0a2ac426fc0fe5e4a90c27dd0043
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-01-27 08:45:36 +0900

    파라미터 제거

--- routers/user.js
+++ routers/user.js
@@ -4,18 +4,18 @@ const swaggerValidation = require('../util/validator');
 const userControls = require('../controllers/userControllers.js')
 const upload = require('../util/multerHelper').profileUpload;
 
-router.all('*'  ,cors());
+router.all('*',cors());
 
-router.post('/sign-in', swaggerValidation.validate, (req, res) => userControls.signIn(req, res));
-router.post('/social', swaggerValidation.validate, (req, res) => userControls.social(req, res));
-router.post('', swaggerValidation.validate, (req, res) => userControls.register(req, res));
-router.post('/check', swaggerValidation.validate, (req, res) => userControls.checkUserId(req, res));
-router.get('/:id', swaggerValidation.validate, (req, res) => userControls.getUserInfo(req, res));
-router.put('/name', swaggerValidation.validate, (req, res) => userControls.changeUserName(req, res));
-router.put('/image', upload.single('profileImg'), swaggerValidation.validate, (req, res) => userControls.changeUserImage(req, res));
-router.put('/password', swaggerValidation.validate, (req, res) => userControls.changePassword(req, res));
-router.put('/password-temp', swaggerValidation.validate, (req, res) => userControls.temporaryPassword(req, res));
-router.put('/sign-out', swaggerValidation.validate, (req, res) => userControls.signOut(req, res));
-router.delete('', swaggerValidation.validate, (req, res) => userControls.withdrawal(req, res));
+router.post('/sign-in', swaggerValidation.validate, userControls.signIn);
+router.post('/social', swaggerValidation.validate, userControls.social);
+router.post('', swaggerValidation.validate, userControls.register);
+router.post('/check', swaggerValidation.validate, userControls.checkUserId);
+router.get('/:id', swaggerValidation.validate, userControls.getUserInfo);
+router.put('/name', swaggerValidation.validate, userControls.changeUserName);
+router.put('/image', upload.single('profileImg'), swaggerValidation.validate, userControls.changeUserImage);
+router.put('/password', swaggerValidation.validate, userControls.changePassword);
+router.put('/password-temp', swaggerValidation.validate, userControls.temporaryPassword);
+router.put('/sign-out', swaggerValidation.validate, userControls.signOut);
+router.delete('', swaggerValidation.validate, userControls.withdrawal);
 
 module.exports = router;
