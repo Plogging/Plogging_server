9f134ca22a56fb7c826490a2dc474b08c383579e
kimCoding <xowns4817@naver.com>
2021-02-02 22:33:38 +0900

    pr review 반영

--- controllers/ploggingControllers.js
+++ controllers/ploggingControllers.js
@@ -4,9 +4,10 @@ const ploggingFilePath = process.env.IMG_FILE_PATH + "/plogging/";
 const PloggingSchema = require('../models/plogging');
 const RankSchema = require('../models/ranking');
 const User = require('../models/users.js');
-const {sequelize} = require('../models/index');
+const { sequelize } = require('../models/index');
 const logger = require("../util/logger.js")("plogging.js");
 const logHelper = require("../util/logHelper.js");
+const pagingHelper = require('../util/pagingHelper');
 
 /**
  * 산책 이력조회  (페이징 처리 필요)
@@ -14,7 +15,7 @@ const logHelper = require("../util/logHelper.js");
  *  case 2 .플로깅 점수순
  *  case 2. 플로깅 거리순
  */
-const readPlogging = async function(req, res) {
+const readPlogging = async function (req, res) {
     logger.info("plogging read api !");
     logger.info(logHelper.reqWrapper(req, "plogging"));
 
@@ -25,25 +26,25 @@ const readPlogging = async function(req, res) {
     let currentPageNumber = req.query.pageNumber; // 조회할 페이지 Number
 
     // default -> 각 페이지에 4개씩, 1번 페이지 조회
-    if(!ploggingCntPerPage) ploggingCntPerPage = 4;
-    if(!currentPageNumber) currentPageNumber = 1;
+    if (!ploggingCntPerPage) ploggingCntPerPage = 4;
+    if (!currentPageNumber) currentPageNumber = 1;
 
     let searchType = Number(req.query.searchType); // 최신순(0), 점수순(1), 거리순(2)
-    let query = {"meta.user_id": targetUserId};
-    let sort_option = [{"meta.created_time": -1},
-                       {"meta.plogging_total_score": -1},
-                       {"meta.distance": -1}];  
+    let query = { "meta.user_id": targetUserId };
+    let sort_option = [{ "meta.created_time": -1 },
+    { "meta.plogging_total_score": -1 },
+    { "meta.distance": -1 }];
     let options = {
         sort: sort_option[searchType],
-        skip: (currentPageNumber-1)*ploggingCntPerPage,
+        skip: (currentPageNumber - 1) * ploggingCntPerPage,
         limit: ploggingCntPerPage
     }
 
     const [allPloggingCount, plogging_list] = await PloggingSchema.readPloggingModel(query, options, targetUserId);
-    
-    const meta = { };
-    meta.startPageNumber=1;
-    meta.endPageNumber = calcLastPage(allPloggingCount, ploggingCntPerPage);
+
+    const meta = {};
+    meta.startPageNumber = 1;
+    meta.endPageNumber = pagingHelper.calcLastPage(allPloggingCount, ploggingCntPerPage);
     meta.currentPageNumber = currentPageNumber;
 
     returnResult.meta = meta;
@@ -56,7 +57,7 @@ const readPlogging = async function(req, res) {
  * 산책 이력 등록
  * - img는 optional. 만약, 입력안하면 baseImg로 세팅
  */
-const writePlogging = async function(req, res) {
+const writePlogging = async function (req, res) {
     logger.info("plogging write api !");
     logger.info(logHelper.reqWrapper(req, "plogging"));
 
@@ -66,7 +67,7 @@ const writePlogging = async function(req, res) {
     ploggingObj = JSON.parse(ploggingObj);
 
     ploggingObj.meta.user_id = userId;
-    ploggingObj.meta.created_time = util.getCurrentDateTime(); 
+    ploggingObj.meta.created_time = util.getCurrentDateTime();
 
     const ploggingScoreArr = calcPloggingScore(ploggingObj);
     const ploggingTotalScore = Number(ploggingScoreArr[0] + ploggingScoreArr[1]);
@@ -76,33 +77,33 @@ const writePlogging = async function(req, res) {
     ploggingObj.meta.plogging_total_score = ploggingTotalScore;
 
     //이미지가 없을때는 baseImg insert
-    if(req.file===undefined) ploggingObj.meta.plogging_img = `${process.env.SERVER_REQ_INFO}/plogging/baseImg.PNG`;
+    if (req.file === undefined) ploggingObj.meta.plogging_img = `${process.env.SERVER_REQ_INFO}/plogging/baseImg.PNG`;
     else ploggingObj.meta.plogging_img = process.env.SERVER_REQ_INFO + '/' + req.file.path.split(`${process.env.IMG_FILE_PATH}/`)[1];
 
-        await sequelize.transaction(async (t) => {
-            const userData = await User.findOneUser(userId, t);
-            const updatedPloggingData = { };
-            // 주간
-            updatedPloggingData.scoreWeek = userData.score_week + ploggingTotalScore;
-            updatedPloggingData.distanceWeek = userData.distance_week + ploggingDistance;
-            updatedPloggingData.trashWeek = userData.trash_week + pickCount;
-            
-            // 월간
-            updatedPloggingData.scoreMonth = userData.score_month + ploggingTotalScore;
-            updatedPloggingData.distanceMonth = userData.distance_month + ploggingDistance;
-            updatedPloggingData.trashMonth = userData.trash_month + pickCount;
+    await sequelize.transaction(async (t) => {
+        const userData = await User.findOneUser(userId, t);
+        const updatedPloggingData = {};
+        // 주간
+        updatedPloggingData.scoreWeek = userData.score_week + ploggingTotalScore;
+        updatedPloggingData.distanceWeek = userData.distance_week + ploggingDistance;
+        updatedPloggingData.trashWeek = userData.trash_week + pickCount;
 
-            // mariadb update
-            await User.updateUserPloggingData(updatedPloggingData, userId, t);
+        // 월간
+        updatedPloggingData.scoreMonth = userData.score_month + ploggingTotalScore;
+        updatedPloggingData.distanceMonth = userData.distance_month + ploggingDistance;
+        updatedPloggingData.trashMonth = userData.trash_month + pickCount;
 
-            // mongodb update
-            await PloggingSchema.writePloggingModel(ploggingObj);
+        // mariadb update
+        await User.updateUserPloggingData(updatedPloggingData, userId, t);
 
-            // redis update
-            await RankSchema.update(userId, ploggingTotalScore);
+        // mongodb update
+        await PloggingSchema.writePloggingModel(ploggingObj);
 
-            res.status(200).json(returnResult);
-        });
+        // redis update
+        await RankSchema.update(userId, ploggingTotalScore);
+
+        res.status(200).json(returnResult);
+    });
 }
 
 /*
@@ -110,7 +111,7 @@ const writePlogging = async function(req, res) {
  *  ->산책이력의 objectId값을 파라미터로 전달
  *   
  */
-const deletePlogging = async function(req, res) {
+const deletePlogging = async function (req, res) {
     logger.info("plogging delete api !");
     logger.info(logHelper.reqWrapper(req, "plogging"));
 
@@ -118,17 +119,17 @@ const deletePlogging = async function(req, res) {
     let userId = req.userId;
     let ploggingId = req.query.ploggingId;
     let ploggingImgName = req.query.ploggingImgName; // plogging_20210106132743.PNG
-    let ploggingImgPath = `${ploggingFilePath}${userId}/${ploggingImgName}`; 
+    let ploggingImgPath = `${ploggingFilePath}${userId}/${ploggingImgName}`;
 
     // 산책이력 삭제
     PloggingSchema.deletePloggingModel(ploggingId);
-            
+
     // 산책이력 이미지 삭제
-    if(fs.existsSync(ploggingImgPath)) fs.unlinkSync(ploggingImgPath);
+    if (fs.existsSync(ploggingImgPath)) fs.unlinkSync(ploggingImgPath);
     res.status(200).json(returnResult);
 }
 
-const getPloggingScore = async function(req, res) {
+const getPloggingScore = async function (req, res) {
     logger.info("plogging getScore api !");
     logger.info(logHelper.reqWrapper(req, "plogging"));
 
@@ -140,10 +141,10 @@ const getPloggingScore = async function(req, res) {
     // 해당 산책의 plogging 점수
     const ploggingScoreArr = calcPloggingScore(ploggingObj);
     const ploggingTotalScore = Number(ploggingScoreArr[0] + ploggingScoreArr[1]);
-    const ploggingActivityScore =  Number(ploggingScoreArr[0]);
+    const ploggingActivityScore = Number(ploggingScoreArr[0]);
     const ploggingEnvironmentScore = Number(ploggingScoreArr[1]);
 
-    returnResult.score = { };
+    returnResult.score = {};
     returnResult.score.totalScore = ploggingTotalScore;
     returnResult.score.activityScore = ploggingActivityScore;
     returnResult.score.environmentScore = ploggingEnvironmentScore;
@@ -152,52 +153,46 @@ const getPloggingScore = async function(req, res) {
 
 // 산책 점수 계산 ( 운동점수, 환경점수 )
 function calcPloggingScore(ploggingObj) {
-    let score = [ ]; // score[0]: 운동점수, score[1]: 환경점수
+    let score = []; // score[0]: 운동점수, score[1]: 환경점수
     const pivotDistance = 300; // 300m
     const movePerScore = 1; // 10m 이동시 1점 증가
     const maxCountDistance = 10000; // 10km
     const pickPerScore = 10; // 쓰레기 1개 주울때마다 10점 증가
-    
+
     const distance = ploggingObj.meta.distance; // 플로깅 거리
     const pickList = ploggingObj.pick_list; // 주운 쓰레기 리스트
-    
-    if(distance < pivotDistance) score[0] = 0; //300m 이하는 거리점수 없음
+
+    if (distance < pivotDistance) score[0] = 0; //300m 이하는 거리점수 없음
     else {
-        if(maxCountDistance < distance) distance = maxCountDistance; // 10km 넘어가면 그 이상 거리점수 없음
-        score[0] = ((Math.floor(distance/10))*movePerScore) + addExtraScorePerKm(distance);
+        if (maxCountDistance < distance) distance = maxCountDistance; // 10km 넘어가면 그 이상 거리점수 없음
+        score[0] = ((Math.floor(distance / 10)) * movePerScore) + addExtraScorePerKm(distance);
     }
 
-    let pickCount=0;
-    for(let i=0; i<pickList.length; i++) pickCount += pickList[i].pick_count;
-    score[1]= pickCount*pickPerScore;
+    let pickCount = 0;
+    for (let i = 0; i < pickList.length; i++) pickCount += pickList[i].pick_count;
+    score[1] = pickCount * pickPerScore;
 
     return score;
 };
 
 // 1km 마다 기본점수 폭 늘려준다. 해당 거리의 경우 추가되는 총 점수
 function addExtraScorePerKm(distance) {
-    const hopCnt = Math.floor(distance/1000);
-    let extraScore=0;
-    for(let i=1; i<=hopCnt; i++) extraScore += (i*10);
+    const hopCnt = Math.floor(distance / 1000);
+    let extraScore = 0;
+    for (let i = 1; i <= hopCnt; i++) extraScore += (i * 10);
     return extraScore;
 }
 
 // 쓰레기 주운갯수 계산
 function calPickCount(pickList) {
-    let pickCount=0;
-    for(let i=0; i<pickList.length; i++) pickCount += pickList[i].pick_count;
+    let pickCount = 0;
+    for (let i = 0; i < pickList.length; i++) pickCount += pickList[i].pick_count;
     return pickCount;
 }
 
-// 마지막 페이지 수 계산
-function calcLastPage(allPloggingCount, ploggingCntPerPage) {
-    if(allPloggingCount % ploggingCntPerPage === 0) return allPloggingCount/ploggingCntPerPage;
-    else return Math.floor(allPloggingCount/ploggingCntPerPage) + 1;
-}
-
 module.exports = {
-   readPlogging,
-   writePlogging,
-   deletePlogging,
-   getPloggingScore
+    readPlogging,
+    writePlogging,
+    deletePlogging,
+    getPloggingScore
 };
