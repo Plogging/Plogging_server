a2c38f8611d8ef41042f3b1bd8198100fe2302d3
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-02-06 09:53:52 +0900

    response additional userInfo from DB

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -112,9 +112,12 @@ const getUserInfo = async(req, res) => {
     returnResult.userId = user.user_id;
     returnResult.userImg = user.profile_img;
     returnResult.userName = user.display_name;
-    returnResult.userScore = user.score;
-    returnResult.userDistance = user.distance;
-    returnResult.userTrash = user.trash;
+    returnResult.scoreMonthly = user.score_month;
+    returnResult.distanceMonthly = user.distance_month;
+    returnResult.trashMonthly = user.trash_month;
+    returnResult.scoreWeekly = user.score_week;
+    returnResult.distanceWeekly = user.distance_week;
+    returnResult.trashWeekly = user.trash_week;
     res.json(returnResult);
 }
 
