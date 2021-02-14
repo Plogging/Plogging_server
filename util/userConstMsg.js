7199ec0232579679143462e3950839ff031a54e0
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-02-14 11:42:06 +0900

    파일 이름 변경

---
+++ util/userConstMsg.js
@@ -0,0 +1,13 @@
+module.exports = {
+    EXISTED_ID: '이미 사용중인 아이디입니다.',
+    EXISTED_NAME: '이미 사용중인 닉네임입니다.',
+    ERR_AUTHORIZATION: '가입되지 않은 정보이거나 비밀번호가 다릅니다.',
+    ERR_EMAIL: '유효하지 않은 이메일입니다.',
+    ERR_SERVER: '서버에 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
+    ERR_PASSWORD: '현재 비밀번호가 일치하지 않습니다.',
+    NOT_FOUND_USER_ID: '아이디를 찾지 못했습니다.',
+    SUCCESS: 'OK',
+    CREATED: '생성되었습니다.',
+    ERR_PASSWORD_COUNT: '로그인 오류 횟수를 초과했습니다. 잠시후 다시 로그인해주세요.',
+    ERR_PASSWORD_ALERT: '한 번더 잘못 입력시 30분간 로그인이 제한됩니다.'
+}
