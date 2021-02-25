60704b05b056e11d50d21800f18b306b253c9e51
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-02-26 00:44:04 +0900

    암호화 구현

--- start.config.js
+++ start.config.js
@@ -16,13 +16,13 @@ module.exports = {
               "MONGODB_PASSWORD": "JjaoSaoP+IRVDBkXdDR4XMgTmkMut0VM",
               "REDIS_INFO": "127.0.0.1:6379", 
               "REDIS_PASSWORD":"JjaoSaoP+IRVDBkXdDR4XMgTmkMut0VM",
-              "MARIADB_INFO": "127.0.0.1:3306", 
+              "MARIADB_INFO": "127.0.0.1:3306",
               "MARIADB_PASSWORD": "JjaoSaoP+IRVDBkXdDR4XMgTmkMut0VM",
-              "IMG_FILE_PATH": "/mnt/Plogging_server/images",
+              "IMG_FILE_PATH": "E:file_test",
               "ADMIN_EMAIL_ID": "ploggingteam@gmail.com",
-              "ADMIN_EMAIL_PASSWORD": "murder^^6",
+              "ADMIN_EMAIL_PASSWORD": "1/KvPFPwoNiXJhstP/+J7xF4Jrvh0X2t",
               "SERVER_REQ_INFO": "http:127.0.0.1:20000",
-              "LOG_PATH": "/mnt/Plogging_server/images/log"
+              "LOG_PATH": "E:\winston-log"
           },
           env_development: { // tb ( 라즈베리파이 서버 )
             "NODE_ENV": "development",
@@ -35,8 +35,8 @@ module.exports = {
             "MARIADB_PASSWORD": "JjaoSaoP+IRVDBkXdDR4XMgTmkMut0VM",
             "IMG_FILE_PATH": "/mnt/Plogging_server/images",
             "SERVER_REQ_INFO": "https:nexters.plogging.kro.kr:20000",
-            "ADMIN_EMAIL_ID": "ploggingteam@gamil.com",
-            "ADMIN_EMAIL_PASSWORD": "murder^^6",
+            "ADMIN_EMAIL_ID": "ploggingteam@gmail.com",
+            "ADMIN_EMAIL_PASSWORD": "1/KvPFPwoNiXJhstP/+J7xF4Jrvh0X2t",
             "LOG_PATH": "/mnt/Plogging_server/images/log"
           },
           env_production: { // 상용 ( 클라우드 서버 )
