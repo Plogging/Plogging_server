ba14b528c9e1868cb8a5d24694aaa41059a3d019
kimCoding <xowns4817@naver.com>
2021-03-14 12:23:57 +0900

    start.sh 파일수정 및 test스크립트제거

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ docker rm plogging-app || true
 docker rmi plogging-app-img || true
 
 if [ "${branch}" = "develop" ]; then
-	docker build -t plogging-app-img .dev.Dockerfile
+	docker build -t plogging-app-img ./dev.Dockerfile
 	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
 elif [ "${branch}" = "master" ]; then
 	docker build -t plogging-app-img ./prod.Dockerfile
