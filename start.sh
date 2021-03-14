8a794146746587921c4e0652630fd3b8574a2627
kimCoding <xowns4817@naver.com>
2021-03-14 12:04:53 +0900

    브랜치 별로 start.sh 배포 스크립트 분리

--- start.sh
+++ start.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 echo 'plogging-server start'
 
-git pull origin develop
+branch=$1
+
+echo deploy ${branch}
+
+git pull origin ${branch}
 
 docker stop plogging-app || true
 docker rm plogging-app || true
 docker rmi plogging-app-img || true
 
-docker build -t plogging-app-img .
-docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
+if [ "${branch}" = "develop" ]; then
+	docker build -t plogging-app-img .dev.Dockerfile
+	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
+elif [ "${branch}" = "master" ]; then
+	docker build -t plogging-app-img ./prod.Dockerfile
+	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data_1/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
+else 
+	echo "please check branchName"
+fi
+
