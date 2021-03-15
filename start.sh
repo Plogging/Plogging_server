f561e9cfbef82242c2a84f37769c5025c27f29fa
kimCoding <xowns4817@naver.com>
2021-03-15 21:27:03 +0900

    Dockerfile 디렉토리로 관리

--- start.sh
+++ start.sh
@@ -12,10 +12,10 @@ docker rm plogging-app || true
 docker rmi plogging-app-img || true
 
 if [ "${branch}" = "develop" ]; then
-	docker build -t plogging-app-img ./dev.Dockerfile
+	docker build -t plogging-app-img ./Dockerfile/dev.Dockerfile
 	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
 elif [ "${branch}" = "master" ]; then
-	docker build -t plogging-app-img ./prod.Dockerfile
+	docker build -t plogging-app-img ./Dockerfile/prod.Dockerfile
 	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data_1/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
 else 
 	echo "please check branchName"
