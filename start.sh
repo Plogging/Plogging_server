0792e455e80fdce49c33c28cb6911da32cf305f1
kimCoding <xowns4817@naver.com>
2021-03-15 21:38:36 +0900

    start.sh 수정

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 echo 'plogging-server start'
 
 branch=$1
+ip=$2
 
 echo deploy ${branch}
 
@@ -12,11 +13,20 @@ docker rm plogging-app || true
 docker rmi plogging-app-img || true
 
 if [ "${branch}" = "develop" ]; then
+	echo "asus server !"
 	docker build -t plogging-app-img ./Dockerfile/dev.Dockerfile
 	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
 elif [ "${branch}" = "master" ]; then
 	docker build -t plogging-app-img ./Dockerfile/prod.Dockerfile
-	docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data_1/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
+	if [ "${ip}" = "192.168.0.17" ]; then
+        echo "intel nuc Server !"
+		docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data_1/ploggingImgs:/mnt/Plogging_server/images plogging-app-img	
+    elif [ "${ip}" = "192.168.0.9" ]; then 
+        echo "Rasberry Pi Server !"
+		docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /home/ubuntu/exhard/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
+    else 
+        echo "please check Server Ip"
+    fi
 else 
 	echo "please check branchName"
 fi
