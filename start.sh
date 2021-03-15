cef14e2e20a983c62a7403bf9021838b0c7a6241
kimCoding <xowns4817@naver.com>
2021-03-15 21:48:23 +0900

    start.sh indent 수정

--- start.sh
+++ start.sh
@@ -19,14 +19,14 @@ if [ "${branch}" = "develop" ]; then
 elif [ "${branch}" = "master" ]; then
 	docker build -t plogging-app-img ./Dockerfile/prod.Dockerfile
 	if [ "${ip}" = "192.168.0.17" ]; then
-        echo "intel nuc Server !"
+    	echo "intel nuc Server !"
 		docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /data_1/ploggingImgs:/mnt/Plogging_server/images plogging-app-img	
-    elif [ "${ip}" = "192.168.0.9" ]; then 
-        echo "Rasberry Pi Server !"
+  	elif [ "${ip}" = "192.168.0.9" ]; then 
+  		echo "Rasberry Pi Server !"
 		docker run -it -d -p 8000:8000 --name plogging-app -e APP_ENCRYPTION_PASSWORD=plogging-pw -v /home/ubuntu/exhard/ploggingImgs:/mnt/Plogging_server/images plogging-app-img
-    else 
-        echo "please check Server Ip"
-    fi
+  	else 
+    	echo "please check Server Ip"
+  	fi
 else 
 	echo "please check branchName"
 fi
