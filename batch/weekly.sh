55fc53ead1505fe150e03f2e43f9286886632547
kimCoding <xowns4817@naver.com>
2021-03-21 13:46:13 +0900

    batch 파일저장경로 및 파일명 수정

--- batch/weekly.sh
+++ batch/weekly.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $(date '+%Y/%m/%d %H:%M:%S') $(docker exec -it dockercompose_plogging-redis_1 sh -c 'echo "auth dkwmfrjdns1!\nmulti\ndel score_weekly\ndel distance_weekly\ndel trash_weekly\nexec" | redis-cli' | tr '\r\n' ' ') >> ~/flush_weekly.log 2>&1
+today=`date +%Y%m%d%H%M%S`
+
+echo $(date '+%Y/%m/%d %H:%M:%S') $(docker exec -it dockercompose_plogging-redis_1 sh -c 'echo "auth dkwmfrjdns1!\nmulti\ndel score_weekly\ndel distance_weekly\ndel trash_weekly\nexec" | redis-cli' | tr '\r\n' ' ') >> /data/ploggingImgs/batchLog/weekly/flush_${today}.log 2>&1
