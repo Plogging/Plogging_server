de85e1809e90c5305a62ebdd2a9ffedfc34c19a9
gangMac <ganghee@gangMacui-MacBookPro.local>
2021-04-09 22:48:43 +0900

    deprecated 수정

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ const swaggerUi = require('swagger-ui-express');
         logger.error(err.stack);
     });
 
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(bodyParser.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
     app.use(express.static(process.env.IMG_FILE_PATH)); // 정적파일 제공
     // redis sessionStorage 설정
     app.use(session({
